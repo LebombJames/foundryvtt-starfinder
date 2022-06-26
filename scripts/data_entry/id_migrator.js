66187c2b467f84262574b273e05d6748e3f84b7e
LebombJames <77904738+LebombJames@users.noreply.github.com>
2022-06-26 21:27:11 +0100

    Update id_migrator.js

--- scripts/data_entry/id_migrator.js
+++ scripts/data_entry/id_migrator.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
-const oldPath = "E:\\User Files\\Documents\\GitHub\\foundryvtt-starfinder\\src\\items\\equipment"
+const oldPath = "E:\\User Files\\Documents\\GitHub\\foundryvtt-starfinder\\src\\items\\equipment";
 const newPath = ".";
-let newFilesMapped = []
-let oldFilesMapped = []
-let oldFile
-
 
 let sanitize = null;
 try {
@@ -12,10 +8,10 @@ try {
 } catch (err) {   
 }   
 
-let newFiles = fs.readdirSync(newPath, 'utf8')
-let oldFiles = fs.readdirSync(oldPath, 'utf8')
+let newFiles = fs.readdirSync(newPath, 'utf8');
+let oldFiles = fs.readdirSync(oldPath, 'utf8');
 
-newFilesMapped = newFiles.map(i => {
+let newFilesMapped = newFiles.map(i => {
     if (i.split('.').pop() === "json") {
         let json = fs.readFileSync(`${newPath}/${i}`, {
             encoding: 'utf8',
@@ -24,11 +20,11 @@ newFilesMapped = newFiles.map(i => {
         
         return JSON.parse(json);
     } else {
-        return
-    }
-})
+        return;
+    };
+});
     
-oldFilesMapped = oldFiles.map(i => {
+let oldFilesMapped = oldFiles.map(i => {
     if (i.split('.').pop() === "json") {
         let json = fs.readFileSync(`${oldPath}/${i}`, {
             encoding: 'utf8',
@@ -37,31 +33,28 @@ oldFilesMapped = oldFiles.map(i => {
         
         return JSON.parse(json);
     } else {
-        return
-    }
-})
+        return;
+    };
+});
 
 for (let newFile of newFilesMapped) {
     try {
-        oldFile = oldFilesMapped.find(i => i.name === newFile.name)
+        let oldFile = oldFilesMapped.find(i => i.name === newFile.name);
         
         if (oldFile) {
-            newFile._id = oldFile._id
+            newFile._id = oldFile._id;
             
             const output = JSON.stringify(newFile, null, 2);
                                 
             let filename = newFile.name;
-            if (sanitize) {
-                 filename = sanitize(filename);
-            }
+            if (sanitize) filename = sanitize(filename);
             filename = filename.replace(/[\s]/g, "_");
             filename = filename.replace(/[,;]/g, "");
             filename = filename.toLowerCase();
             
-            console.log(`Migrated ${newFile.name} to Migrated/${filename}.json`)
-            fs.writeFileSync(`Migrated/${filename}.json`, output)
-        }
+            fs.writeFileSync(`Migrated/${filename}.json`, output);
+            console.log(`Migrated ${newFile.name} to Migrated/${filename}.json`);
+        };
     } catch (err) {
-    }
-    
-}
+    };
+};

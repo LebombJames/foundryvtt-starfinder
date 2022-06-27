ffb98d8e8387ea9fd23931630e788a89810c3454
LebombJames <77904738+LebombJames@users.noreply.github.com>
2022-06-27 18:09:10 +0100

    Update id_migrator.js

--- scripts/data_entry/id_migrator.js
+++ scripts/data_entry/id_migrator.js
@@ -8,11 +8,10 @@ try {
 const oldPath = "E:\\User Files\\Documents\\GitHub\\foundryvtt-starfinder\\src\\items\\equipment";
 const newPath = ".";
 
-  
-
 let newFiles = fs.readdirSync(newPath, 'utf8');
 let oldFiles = fs.readdirSync(oldPath, 'utf8');
 
+console.log(`Filtering out non-JSONs from new files...`)
 let newFilesMapped = newFiles.filter(i => i.split('.').pop() === "json").map(i => {
     let json = fs.readFileSync(`${newPath}/${i}`, {
         encoding: 'utf8',
@@ -21,7 +20,8 @@ let newFilesMapped = newFiles.filter(i => i.split('.').pop() === "json").map(i =
     
     return JSON.parse(json);
 });
-    
+
+console.log(`Filtering out non-JSONs from old files...`)
 let oldFilesMapped = oldFiles.filter(i => i.split('.').pop() === "json").map(i => {
     let json = fs.readFileSync(`${oldPath}/${i}`, {
         encoding: 'utf8',
@@ -31,9 +31,10 @@ let oldFilesMapped = oldFiles.filter(i => i.split('.').pop() === "json").map(i =
     return JSON.parse(json);
 });
 
+console.log("Beginning matching...")
 for (let newFile of newFilesMapped) {
     try {
-        let oldFile = oldFilesMapped.find(i => i.name === newFile.name);
+        let oldFile = oldFilesMapped.find(i => i.name.toLowerCase() === newFile.name.toLowerCase());
         
         if (oldFile) {
             newFile._id = oldFile._id;
@@ -46,11 +47,20 @@ for (let newFile of newFilesMapped) {
             filename = filename.replace(/[,;]/g, "");
             filename = filename.toLowerCase();
             
+            if (!fs.existsSync("./Migrated")) {
+                    fs.mkdir("./Migrated", (err) => {
+                        if (err) {
+                            throw err;
+                        };
+                    });
+                };
+            
             fs.writeFileSync(`Migrated/${filename}.json`, output);
             console.log(`Migrated ${newFile.name} to Migrated/${filename}.json`);
         } else {
             console.log(`Didn't find a match for ${newFile.name}!`)
         };
     } catch (err) {
+        console.log(err)
     };
 };
